c9749bf69d2817d6d5466d164a68dac50398c287
Gruzin <gruzd.roman@inbox.ru>
2023-03-16 17:07:19 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -4,4 +4,7 @@ def generate_squares(num: int)-> Dict[int, int]:
     """
     Add your code here or call it from here   
     """
-    pass
+    new_dict = {}
+    for i in range(1, num+1):
+        new_dict[i] = i*i
+    return new_dict
